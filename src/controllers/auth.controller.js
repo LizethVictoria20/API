bff94017f7c51f89ef2b4c319ca558e96ebb563e
LizethVictoria20 <lizvictoria754@gmail.com>
2024-05-29 15:19:42 -0500

    Generando token para register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,16 +1,16 @@
 import { pool } from "../db.js";
+import jwt from "jsonwebtoken";
 
 export const Register = async (req, res) => {
     const {id, name, nationality, isHuman} = req.body;
-    
     const [ rows ] = await pool.query('SELECT * FROM `Character` WHERE id = ? AND name = ? AND nationality = ? AND isHuman = ? ',
                                         [id, name, nationality, isHuman])
-    res.send({
-        id,
-        name,
-        nationality,
-        isHuman 
-    })
+    const data = req.body
+    const newToken = jwt.sign({ data }, 'secret_key', { expiresIn: '1h' });
+            
+    res.json({
+        token: newToken
+    });
 };
 
 export const Login = (req, res) => res.send('Login');
